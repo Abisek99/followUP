d8c0411162d249d6c8aa1fd3e55c217573d1f762
Abhishek <abisekmhrjn@gmail.com>
2023-02-20 19:37:00 +0545

    Add existing project files to Git

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
 const connection = mongoose.connection;
 
-connection.on('connected', () =>{
-    console.log('MongoDB is connected');
+connection.on("connected", () => {
+  console.log("MongoDB is connected");
 });
-connection.on('error', (error) =>{
-    console.log('Error in MongoDB is connection', error);
+connection.on("error", (error) => {
+  console.log("Error in MongoDB is connection", error);
 });
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-
 const express = require("express");
-const app = express();
-require('dotenv').config()
+const dotenv = require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
+//const userRoutes = require("./routes/userRoute");
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+//dotenv config
+//dotenv.config();
+
+//routes
+//app.get("/", (req, res) => res.send("API is Running"));
+app.use("/api/v1/user", require("./routes/userRoutes"));
+
+//port
 const port = process.env.PORT || 5000;
 
-
-
-app.listen(port, () => console.log(`Node server started at port ${port}`));
+//listen port
+app.listen(port, () => {
+  console.log(`Node server started at port ${port}`);
+});
